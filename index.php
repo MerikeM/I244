c6b64dc6dbcb3d3b14bddb232bb82b03b9808124
Merike Meizner <mmeizner@klass2-05-l.intra.itcollege.ee>
2017-02-11 16:08:58 +0200

    Esimese praktikumi failid

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Väike lehekülg</title>
+		<link rel="stylesheet" type="text/css" href="style.css"/>
+	</head>
+	<body>
+	<?php
+	echo phpversion();
+	?>
+	<p>Tere! Ma ei viitsi praegu mingit teksti välja mõelda.</p>
+	<br>
+	<img src="pics/pilt.jpg">
+
+
+	</body>
+</html>
